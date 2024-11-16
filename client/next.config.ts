80d1b1b1f637cd6d64841335d37dad3e6a11eca7
perceptronq <hungrydude283@gmail.com>
2024-11-16 17:21:37 +0530

    chore: update next.config.ts

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,7 +1,16 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+module.exports = {
+  async headers() {
+      return [
+          {
+              // Allow CORS for all API routes
+              source: "/api/:path*",
+              headers: [
+                  { key: "Access-Control-Allow-Credentials", value: "true" },
+                  { key: "Access-Control-Allow-Origin", value: "http://localhost:5000" }, // Your backend URL
+                  { key: "Access-Control-Allow-Methods", value: "GET,POST,OPTIONS" },
+                  { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization" },
+              ]
+          }
+      ]
+  },
+}
